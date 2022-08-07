0f88c01fb14791c7e7f9e770a40e11a990ec2fbc
Yanko08 <97474672+Yanko08@users.noreply.github.com>
2022-08-07 14:57:14 +0300

    Add files via upload

---
+++ bgnTOeuro.js
@@ -0,0 +1,5 @@
+function convert(){
+    const bgnValue = document.getElementById('bgn').value;
+    let sum = bgnValue / 1.95
+    const euroValue = document.getElementById('euro').value = sum.toFixed(2);
+}
